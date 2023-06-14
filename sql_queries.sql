ce04edb4ee0cac958e206ebda59710dd39dac703
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2023-06-14 09:35:54 -0500

    simple JOIN practice

--- sql_queries.sql
+++ sql_queries.sql
@@ -157,3 +157,17 @@ WHERE standard_qty = 0 AND gloss_qty > 1000 OR poster_qty > 1000;
 SELECT *
 FROM accounts
 WHERE (name LIKE 'C%' OR name LIKE 'W%') AND ((primary_poc LIKE ('%ana%') OR primary_poc LIKE '%Ana%') AND primary_poc NOT LIKE '%eana%');
+
+-- JOINS
+-- Try pulling all the data from the accounts table, and all the data from the orders table.
+SELECT *
+FROM accounts
+  JOIN orders
+  ON accounts.id = orders.account_id;
+
+-- Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table.
+SELECT orders.standard_qty, orders.poster_qty,
+  accounts.website, accounts.primary_poc
+FROM orders
+  JOIN accounts
+  ON orders.account_id = accounts.id;
