96c5f8b00b49c7938bdfc3f80a3129cf77686cb8
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2023-06-15 12:02:34 -0500

    more simple JOIN practice

--- sql_queries.sql
+++ sql_queries.sql
@@ -171,3 +171,29 @@ SELECT orders.standard_qty, orders.poster_qty,
 FROM orders
   JOIN accounts
   ON orders.account_id = accounts.id;
+
+-- Provide a table for all web_events associated with the account name of Walmart. There should be three columns. Be sure to include the primary_poc, time of the event, and the channel for each event. Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
+SELECT accounts.primary_poc, web_events.occurred_at, web_events.channel, accounts.name
+FROM web_events
+  JOIN accounts
+  ON web_events.account_id = accounts.id
+WHERE accounts.name = 'Walmart';
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to the account name.
+SELECT region.name AS region_name, sales_reps.name AS rep_name, accounts.name AS account_name
+FROM region
+  JOIN sales_reps
+  ON region.id = sales_reps.region_id
+  JOIN accounts
+  ON sales_reps.id = accounts.sales_rep_id
+ORDER BY account_name;
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. Your final table should have 3 columns: region name, account name, and unit price. A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+SELECT region.name AS region_name, accounts.name AS account_name, ROUND(orders.total_amt_usd / (total + 0.01), 2) AS unit_price
+FROM region
+  JOIN sales_reps
+  ON region.id = sales_reps.region_id
+  JOIN accounts
+  ON sales_reps.id = accounts.sales_rep_id
+  JOIN orders
+  ON accounts.id = orders.account_id;
