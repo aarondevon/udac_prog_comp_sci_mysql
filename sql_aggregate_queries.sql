63819a0efc71f200fc2e1ee7791763495ec40a14
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2023-06-17 21:55:20 -0500

    practice with MIN and MAX

--- sql_aggregate_queries.sql
+++ sql_aggregate_queries.sql
@@ -17,3 +17,47 @@ FROM orders;
 -- Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both aggregation and a mathematical operator.
 SELECT ROUND(SUM(standard_amt_usd) / SUM(standard_qty), 2) AS standard_unit_price
 FROM orders;
+
+-- When was the earliest order ever placed? You only need to return the date.
+SELECT MIN(occurred_at)
+FROM orders;
+
+-- Try performing the same query as in question 1 without using an aggregation function.
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1;
+
+-- When did the most recent (latest) web_event occur?
+SELECT MAX
+(occurred_at)
+FROM web_events;
+
+-- Try to perform the result of the previous query without using an aggregation function.
+SELECT occurred_at
+FROM web_events
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+-- Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+SELECT ROUND
+(AVG
+(standard_amt_usd), 2) AS standard_amt_average,
+ROUND
+(AVG
+(gloss_amt_usd), 2) AS gloss_amt_average,
+ROUND
+(AVG
+(poster_amt_usd), 2) AS poster_amt_average,
+ROUND
+(AVG
+(standard_qty)) AS standard_qty_average,
+ROUND
+(AVG
+(gloss_qty)) AS gloss_qty_average,
+ROUND
+(AVG
+(poster_qty)) AS poster_qty_average
+FROM orders;
+
+-- Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders?
