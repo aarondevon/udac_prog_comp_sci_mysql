ac89ae97e531fb64ca4393ba78c176495621a151
Aaron <10297223+aarondevon@users.noreply.github.com>
2023-06-21 14:01:49 -0500

    add questions to answer

--- sql_aggregate_queries.sql
+++ sql_aggregate_queries.sql
@@ -185,3 +185,12 @@ GROUP BY sales_reps.name
 HAVING COUNT(*) > 5
 ORDER BY sales_reps.name;
 
+-- How many accounts have more than 20 orders?
+-- Which account has the most orders?
+-- Which accounts spent more than 30,000 usd total across all orders?
+-- Which accounts spent less than 1,000 usd total across all orders?
+-- Which account has spent the most with us?
+-- Which account has spent the least with us?
+-- Which accounts used facebook as a channel to contact customers more than 6 times?
+-- Which account used facebook most as a channel?
+-- Which channel was most frequently used by most accounts?
