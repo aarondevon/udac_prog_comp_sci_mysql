f18d313f0a0fa740c1b3552594d99ae4d1485846
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2023-06-26 12:38:27 -0500

    Create a subquery that provides all of the data from the query above

--- sql_subqueries.sql
+++ sql_subqueries.sql
@@ -3,3 +3,10 @@ SELECT channel, DATE_TRUNC('day', occurred_at) AS date, COUNT(*) AS events
 	FROM web_events
 	GROUP BY channel, DATE_TRUNC('day', occurred_at)
     ORDER BY events DESC;
+
+-- Create a subquery that provides all of the data from the query above
+SELECT *
+FROM (SELECT channel, DATE_TRUNC('day', occurred_at) AS date, COUNT(*) AS events
+	FROM web_events
+	GROUP BY channel, DATE_TRUNC('day', occurred_at)
+    ORDER BY events DESC) AS sub;
