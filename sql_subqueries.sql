2ccadff9e86f7dbb44fd1ad83981d315ccdd5fde
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2023-06-26 17:37:00 -0500

    Use DATE_TRUNC to pull month level information about the first order ever placed

--- sql_subqueries.sql
+++ sql_subqueries.sql
@@ -10,3 +10,16 @@ FROM (SELECT channel, DATE_TRUNC('day', occurred_at) AS date, COUNT(*) AS events
 	FROM web_events
 	GROUP BY channel, DATE_TRUNC('day', occurred_at)
     ORDER BY events DESC) AS sub;
+
+-- Find the average number of events from each channel
+SELECT channel, ROUND(AVG(events), 2) AS average_events_per_day
+FROM (SELECT channel, DATE_TRUNC('day', occurred_at) AS date, COUNT(*) AS events
+	FROM web_events
+	GROUP BY channel, DATE_TRUNC('day', occurred_at)
+    ORDER BY events DESC) AS sub
+GROUP BY channel
+ORDER BY average_events_per_day DESC;
+
+-- Use DATE_TRUNC to pull month level information about the first order ever placed
+SELECT DATE_TRUNC('month', MIN(occurred_at))
+FROM orders;
